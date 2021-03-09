2f39c320feb7200e1aad63c5a3121ca8ea564b9c
neoflex-repo <Ne0f1ex>
2021-03-10 01:43:54 +0400

    Task2 add code and report

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
+        String number = "492918472702385";
+        System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
+    }
+
+    public static boolean isValidCardNumber(String number) {
+        if (number == null) {
+            return false;
+        }
+
+        if (number.length() != 16) {
+            return false;
+        }
+
+        long result = 0;
+        for (int i = 0; i < number.length(); i++) {
+            int digit;
+            try {
+                digit = Integer.parseInt(number.charAt(i) + "");
+            } catch (NumberFormatException e) {
+                return false;
+            }
+
+            if (i % 2 == 0) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+            result += digit;
+        }
+
+        return (result != 0) && (result % 10 == 0);
+    }
+}
